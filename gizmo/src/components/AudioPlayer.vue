3ec8a5781264a43101c03e4fe63c0076f6f3cda0
Chris Collins <chris.collins@yaylunch.com>
2023-04-17 20:27:27 -0400

    proof of concept

---
+++ gizmo/src/components/AudioPlayer.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="audio-player">
+      <button @click="playAudio">Play Audio</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      playAudio() {
+        const audio = new Audio(require('@/assets/audio/sample.mp3'));
+        audio.play();
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .audio-player {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  </style>
+  
