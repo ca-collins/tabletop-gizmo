cba7659dcac5dc741b7da96e75b83ceeff5fe321
Chris Collins <chris.collins@yaylunch.com>
2023-04-18 00:46:50 -0400

    test api route working

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/audio/sample', function (Request $request) {
+    return response()->json(['url' => 'https://example.com/sample.mp3']);
+});
+
+Route::get('/audio/{filename}', function (Request $request, $filename) {
+    return Storage::disk('s3')->url($filename);
 });
--- gizmo/src/components/AudioPlayer.vue
+++ gizmo/src/components/AudioPlayer.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="audio-player">
-      <button @click="playAudio">Play Audio</button>
+      <button @click="testBackend">Play Audio</button>
     </div>
   </template>
   
@@ -13,6 +13,23 @@
             const audio = new Audio(sampleAudio);
             audio.play();
         },
+        async playRemoteAudio() {
+            const filename = 'sample.mp3'; // Replace this with the desired filename from the S3 bucket
+            try {
+                const response = await fetch(`http://your-laravel-api-url/api/audio/${filename}`);
+                const audioUrl = await response.text();
+
+                const audio = new Audio(audioUrl);
+                audio.play();
+            } catch (error) {
+                console.error('Error fetching audio URL:', error);
+            }
+        },
+        async testBackend() {
+            const response = await fetch('http://localhost:8000/api/audio/sample');
+            const data = await response.json();
+            console.log(data);
+        }
     },
   };
   </script>
